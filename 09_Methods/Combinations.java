f46e6691ec9e9282a7ee78b13eb8297da86b5287
Aritra Nandi <aritranandi30@gmail.com>
2025-05-02 21:00:48 +0530

    Method of Java - combination

---
+++ 09_Methods/Combinations.java
@@ -0,0 +1,31 @@
+
+
+import java.util.*;
+public class Combinations{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the values of n and r respectively");
+        long n = sc.nextLong();
+        long r = sc.nextLong();
+
+        // if the user enters a negative number, I dont want to calculate the factorial of that
+        if(n < 0 || r < 0){
+            System.out.println("Invalid number");
+            return;
+        }
+
+        long factN = factorial(n), factR = factorial(r), factNR = factorial((n - r));
+        long nCr = factN / (factR * factNR);
+
+        System.out.println(nCr);
+        sc.close();
+    }
+
+    public static long factorial(long num){
+        long factorial = 1l;
+        for(long i = num; i > 0; i--){
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
