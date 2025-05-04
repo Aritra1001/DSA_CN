58c591ae08e7d6420ced547321e93b16c6523a4a
Aritra Nandi <aritranandi30@gmail.com>
2025-05-04 23:48:21 +0530

    for each loop

---
+++ 10_Arrays_1/foreachLoop.java
@@ -0,0 +1,12 @@
+// In for each loop the i represents the elements of the arrays and not the index of the array elements.
+// we can traverse over the values of the arrayh using for each loop
+
+
+public class foreachLoop {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3,53,78,43};
+        for(int i:arr){ 
+            System.out.println(i);
+        }
+    }
+}
