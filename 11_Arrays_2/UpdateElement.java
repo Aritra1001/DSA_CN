42ef4fb593c2fb36a4568f2f7e115f17b9197085
Aritra Nandi <aritranandi30@gmail.com>
2025-05-12 23:48:44 +0530

    Update an element in an array

---
+++ 11_Arrays_2/UpdateElement.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+public class UpdateElement {
+
+    public static int takeInput(String str){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(str);
+        int input = sc.nextInt();
+        return input;
+    }
+
+    public static void replaceKey(int arr[], int key, int newKey){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == key){
+                arr[i] = newKey;
+            }
+            else{
+                System.out.println("Key not found in the array");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = takeInput("Enter the size of the array: ");
+        int arr[] = new int[size];
+        for(int i = 0; i < size; i++){
+            arr[i] = takeInput("Enter the element at " + i + " th index: ");
+        }
+
+        int key = takeInput("Enter the key to find in the array: ");
+
+        int newKey = takeInput("Enter the new key: ");
+
+        replaceKey(arr, key, newKey);
+
+        System.out.print("Updated array: ");
+        for(int i:arr){
+            System.out.print( + i + " ");
+        }
+    }
+}
